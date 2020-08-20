42cf8ca7aae9b8dbc58c0b07a99b326cbf55b6ee
kenneth2001 <yeeki0904@gmail.com>
2020-08-20 19:35:18 +0800

    Update script.py

--- 7 kyu/Lists Filtering/script.py
+++ 7 kyu/Lists Filtering/script.py
@@ -9,6 +9,8 @@ def filter_list(l):
     return lst
 
 # Test
-# print(filter_list([1,2,'a','b']))
-# print(filter_list([1,'a','b',0,15]))
-# print(filter_list([1,2,'aasf','1','123',123]))
+"""
+print(filter_list([1,2,'a','b']))
+print(filter_list([1,'a','b',0,15]))
+print(filter_list([1,2,'aasf','1','123',123]))
+"""
