f5cab67db0a0f3272e3a8f7f5be2a87000463bc9
kenneth2001 <yeeki0904@gmail.com>
2020-08-24 20:43:53 +0800

    Update script.py

--- 6 kyu/Vasya - Clerk/script.py
+++ 6 kyu/Vasya - Clerk/script.py
@@ -1,14 +1,3 @@
-# Description 
-"""
-The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollar bill. An "Avengers" ticket costs 25 dollars.
-
-Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
-
-Can Vasya sell a ticket to every person and give change if he initially has no money and sells the tickets strictly in the order people queue?
-
-Return YES, if Vasya can sell a ticket to every person and give change with the bills he has at hand at that moment. Otherwise return NO.
-"""
-
 def tickets(people):
     cash_25 = 0
     cash_50 = 0
@@ -29,9 +18,3 @@ def tickets(people):
             else:
                 return "NO"    
     return "YES"
-    
-  # Test
-  """
-  print(ticket([25, 25, 50]))  --> output: YES
-  print(ticket([25, 100]))  --> output: NO
-"""
