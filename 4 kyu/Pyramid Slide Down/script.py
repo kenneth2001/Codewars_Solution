e7e39f999047e156abe0edd233f010c193b57b3c
YeeKiiiiii <yeeki0904@gmail.com>
2020-10-02 18:24:07 +0800

    Delete script.py

--- 4 kyu/Pyramid Slide Down/script.py
+++
@@ -1,6 +0,0 @@
-def longest_slide_down(pyramid):
-    value = pyramid.pop()
-    while pyramid:
-        temp = pyramid.pop()
-        value = [temp[i] + max(value[i],value[i+1]) for i in range(len(temp))]
-    return value.pop()
