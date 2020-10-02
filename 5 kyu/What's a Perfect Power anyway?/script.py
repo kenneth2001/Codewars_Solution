4d3f2cba940c4d43e2002bd1568947538e53ed80
YeeKiiiiii <yeeki0904@gmail.com>
2020-10-02 18:27:10 +0800

    Delete script.py

--- 5 kyu/What's a Perfect Power anyway?/script.py
+++
@@ -1,7 +0,0 @@
-from math import log, sqrt
-
-def isPP(n):
-    for k in range(2, round(sqrt(n)+1)):
-        if k ** round(log(n, k)) == n:
-            return [k, round(log(n, k))]
-    return None
