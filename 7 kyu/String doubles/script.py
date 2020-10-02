10235442f16f942b9653a5fe9656dc63984d139f
YeeKiiiiii <yeeki0904@gmail.com>
2020-10-02 18:36:44 +0800

    Delete script.py

--- 7 kyu/String doubles/script.py
+++
@@ -1,8 +0,0 @@
-def doubles(s):
-    string = ""
-    for character in s:
-            string += character
-            if len(string) > 1:
-                if string[-2] == character:
-                    string = string[:-2]  
-    return string
