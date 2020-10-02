35842456cb42bc252ae80ebca2d4ab83530a646b
YeeKiiiiii <yeeki0904@gmail.com>
2020-10-02 18:29:38 +0800

    Delete script.py

--- 6 kyu/Persistent Bugger/script.py
+++
@@ -1,7 +0,0 @@
-from functools import reduce
-def persistence(n):
-    count = 0
-    while n >= 10:
-        n = reduce(lambda x, y: x * y, [int(i) for i in str(n)])
-        count += 1
-    return count
