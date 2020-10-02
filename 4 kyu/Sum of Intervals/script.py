59b6b7237888d213eb9d0e63579ee800e66a8158
YeeKiiiiii <yeeki0904@gmail.com>
2020-10-02 18:25:04 +0800

    Delete script.py

--- 4 kyu/Sum of Intervals/script.py
+++
@@ -1,7 +0,0 @@
-def sum_of_intervals(intervals):
-    lst = []
-    for array in intervals:
-        for num in range(array[0], array[1]):
-            if num not in lst:
-                lst.append(num)
-    return len(lst)
