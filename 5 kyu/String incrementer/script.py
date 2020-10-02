8c205a4d4d5f5ec08161adc89bee6b6168b8f03f
YeeKiiiiii <yeeki0904@gmail.com>
2020-10-02 18:26:52 +0800

    Delete script.py

--- 5 kyu/String incrementer/script.py
+++
@@ -1,6 +0,0 @@
-def increment_string(string):
-    word_of_string = string.rstrip("0123456789")
-    word_of_integer = string[len(word_of_string):]
-    if word_of_integer == "":
-        return word_of_string + "1"
-    return word_of_string + str(int(word_of_integer) + 1).zfill(len(word_of_integer))
