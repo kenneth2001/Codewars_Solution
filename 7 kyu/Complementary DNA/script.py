1cefe04cc5bce391d7f6f46aa3aa870d62758e03
YeeKiiiiii <yeeki0904@gmail.com>
2020-10-02 18:29:48 +0800

    Delete script.py

--- 7 kyu/Complementary DNA/script.py
+++
@@ -1,6 +0,0 @@
-def DNA_strand(dna):
-    my_table = str.maketrans("ATCG", "TAGC")
-    return dna.translate(my_table)
-
-#def DNA_strand(dna):
-    #return dna.translate(str.maketrans('ATCG', 'TAGC')) 
