f4babdfcea497cd4651ae4be2ca4dd9ed3e71e27
yeekiiiiii <>
2022-01-17 18:43:36 +0800

    Upload solution for Kata "Return Negative"

--- Solution/7 kyu/Sum of the first nth term of Series/script.js
+++ Solution/7 kyu/Sum of the first nth term of Series/script.js
@@ -1,10 +1,7 @@
-function SeriesSum(n){
+function SeriesSum(n) {
     let ans = 0.0
-    for(let i=0; i<n; i++){
-      ans += 1.0 / (1 + 3 * i)
+    for (let i = 0; i < n; i++) {
+        ans += 1.0 / (1 + 3 * i)
     }
-
     return ans.toFixed(2)
-  }
-
-console.log(SeriesSum(2))
+}
